d17bd887968280dfa5d955b5e3aab16298ddaa43
Paulo Henrique <paulo24041996@gmail.com>
2024-10-20 18:42:41 -0300

    fix: Error server.js

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Middleware para servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota para a página inicial
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+        if (err) {
+            res.status(err.status).end();
+        }
+    });
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
